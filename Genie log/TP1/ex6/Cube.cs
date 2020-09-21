15e4a7d524afa3e662b3cd0c36ba659afc1542b2
Antoine Parize <aparize@ensc.fr>
2020-09-21 11:55:13 +0200

    fin TD2, début TD3

---
+++ Genie log/TP1/ex6/Cube.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex6
+{
+    class Cube:IZoomable
+    {
+        public double Cote { get; set; }
+
+        public Cube(double cote)
+        {
+            Cote = cote;
+        }
+        public void zoom(double z)
+        {
+            Cote = Cote * z;
+        }
+
+        public double volume()
+        {
+            return Cote * Cote * Cote;
+        }
+        public double aire()
+        {
+            return Cote * Cote * 6;
+        }
+    }
+}
---
+++ Genie log/TP1/ex6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex6
+{
+    interface IZoomable
+    {
+        void zoom(double coef);
+        double volume();
+        double aire();
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cube c = new Cube(2);
+            Console.WriteLine(c.Cote);
+            Console.WriteLine(c.volume());
+            Console.WriteLine(c.aire());
+            Console.ReadKey();
+        }
+    }
+}
