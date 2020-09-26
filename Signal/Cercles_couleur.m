2f0eda7126c859a1a129faa45d63ad0ea4f4b6e0
Antoine PARIZE <45354189+lejugeti@users.noreply.github.com>
2020-09-26 16:58:21 +0200

    Update Cercles_couleur.m

--- Signal/Cercles_couleur.m
+++ Signal/Cercles_couleur.m
@@ -4,17 +4,15 @@ clc
 s =200;
 m = s/2;
 R=70;
-I = [zeros(s);zeros(s)];
 
-centre = [s/2;s/2];
 d = 50; %distance des cercles au centre
-cr = [0;-d];
-cb = d*[-cos(pi/6); sin(pi/6)];
-cg = d*[cos(pi/6); sin(pi/6)];
+cr = [0;-d]; %centre rouge
+cb = d*[-cos(pi/6); sin(pi/6)]; %centre bleu
+cg = d*[cos(pi/6); sin(pi/6)]; %centre vert
 
 [X,Y] = meshgrid(-s/2:s/2);
 
-a = (X+cr(1,1)).^2 + (Y-cr(2,1)).^2 <= R^2;
+a = (X - cr(1,1)).^2 + (Y - cr(2,1)).^2 <= R^2;
 b = (X - cb(1,1)).^2 + (Y - cb(2,1)).^2 <= R^2;
 c = (X - cg(1,1)).^2 + (Y - cg(2,1)).^2 <= R^2;
 imagesc(a);
