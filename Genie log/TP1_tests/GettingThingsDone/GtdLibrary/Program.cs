da3a09f101038824c103fa9b11a567cab3f3e10c
Antoine PARIZE <lejugeti@gmail.com>
2020-10-01 18:54:33 +0200

    Debut TP1 tests

---
+++ Genie log/TP1_tests/GettingThingsDone/GtdLibrary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtdLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime a = new DateTime(2020, 2, 10);
+            DateTime b = new DateTime(2020, 2, 20);
+            Console.WriteLine(a.AddDays(2));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Genie log/TP1_tests/GettingThingsDone/GtdLibrary/Task.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtdLibrary
+{
+    class Task
+    {
+        private int _repetition;
+        protected string Nom { get; set; }
+        protected virtual DateTime Today 
+        {
+            get
+            {
+                return DateTime.Today;
+            }
+        }
+        protected DateTime Echeance
+        {
+            get; set;
+        }
+        protected bool Done { get; set; }
+        protected int Repetition
+        {
+            get { return _repetition; }
+            set
+            {
+                if (value < 0) _repetition = 0;
+                else _repetition = value;
+            }
+        }
+
+        protected int JoursAvantEcheance()
+        {
+            return (Echeance - Today).Days;
+        }
+
+        public void TacheTerminee()
+        {
+            if (Repetition != 0)
+            {
+                Echeance.AddDays(Repetition);
+            }
+            else Done = true;
+        }
+    }
+}
