25b6b9bfc9392128cf8f8d0a581d092e81d6335e
Antoine PARIZE <45354189+lejugeti@users.noreply.github.com>
2020-10-01 11:47:28 +0200

    TD Voiture

---
+++ Genie log/ConsoleApplication1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Voiture v = new Voiture(4, "1234");
+        double cout = v.Rouler(100, 800);
+        Console.WriteLine(cout);
+        Console.ReadKey();
+    }
+}
---
+++ Genie log/ModelingProject1Lib/GeneratedCode/CodeFile1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Voiture v = new Voiture(4, "1234");
+        Console.WriteLine(v.Rouler(100, 100));
+        Console.ReadKey();
+    }
+}
