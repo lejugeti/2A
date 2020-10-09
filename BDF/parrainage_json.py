3235d67dc37c470676f101f2725ea54fe92f1c4e
Antoine PARIZE <lejugeti@gmail.com>
2020-10-09 17:49:13 +0200

    Fin script parrainage

--- BDF/parrainage_json.py
+++ BDF/parrainage_json.py
@@ -1,11 +1,17 @@
+#%%
 import pandas as pd
 import re 
+import os
 
-path = "H:\\rep.xlsx"
-rep = pd.read_excel(path)
+path = "E:/Cours/ensc/2A/BDF/"
+rep = pd.read_excel(path + "rep.xlsx")
 
 rep.drop(columns="Horodateur", inplace=True)
-rep.columns = ["nom", "prenom", "famille", "participe", "path", "message", "contact"]
+rep.columns = ["nom_2A", "prenom_2A", "famille", "participe", "path", "message", "contact"]
+
+#on met tous les noms en upper + drop prénoms
+rep.nom_2A = rep.nom_2A.str.upper()
+rep.drop(columns="prenom_2A", inplace=True)
 
 #on change les réponses de participation pour que ça soit plus facilement utilisable
 p = rep["participe"]
@@ -14,15 +20,56 @@ p[p=="Oui monsieur"] = "oui"
 rep["participe"] = p
 
 #on check les numéros de tel
-pattern = "^0[6-7][0-9]{8}$"
+patternNum = "^0[6-7][0-9]{8}$"
 
 for i in range(len(rep)):
-    testNum = re.match(pattern, rep["contact"][i])
+    testNum = re.match(patternNum, rep["contact"][i])
 
     if(not testNum):
-        rep["contact"][i] = "null"
+        rep["contact"][i] = "aucun" 
 
-
-rep.to_json("H:\\rep.json", orient="index")
+#rep.contact = rep.contact.astype(str)
+#rep.to_json(path + "rep.json", orient="index")
 
 print(rep)
+
+#%% join des tableaux
+
+liste1A = pd.read_excel("E:/Cours/ensc/2A/BDF/1A.xlsx")
+
+finalListe = pd.merge(liste1A, rep, on="nom_2A")
+finalListe.to_excel(path + "liste_merged.xlsx")
+print(finalListe)
+
+#%% ajout des paths photos
+
+df = pd.read_excel(os.path.join(path, "liste_merged.xlsx"))
+photos = os.listdir(os.path.join(path, "photos"))
+
+for line in df.iterrows():
+    lineData = line[1] #pour accéder aux données du Series
+    
+    changementPath = False
+    tempNom = lineData.nom_2A.lower()
+    tempPrenom = lineData.prenom_2A.lower()
+    patternPhoto = f"{tempNom}"
+
+    for p in photos:
+        pName, pExt = os.path.splitext(p)
+        pName = pName.lower()
+        match = re.search(patternPhoto, pName)
+        
+        if(match):
+            newPath = os.path.join(path, "photos", f"{tempNom}_{tempPrenom}{pExt}")
+            os.rename(os.path.join(path, "photos", p), newPath)
+            df.path[line[0]] = os.path.join(f"./photos/{tempNom}_{tempPrenom}{pExt}")
+            changementPath = True
+    
+    #on enlève le path google drive s'il n'y a pas de photo fournie
+    if(not changementPath): 
+        df.path[line[0]] = "null"
+    
+#%% écriture du tableau final
+
+df.to_excel(os.path.join(path, "liste_merged.xlsx"))
+df.to_json(os.path.join(path, "liste_parrainage.json"), orient="index")
