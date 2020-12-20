79bcc9bc61de44371dab8ef75628ca88426cb185
Antoine PARIZE <lejugeti@gmail.com>
2020-12-20 12:35:34 +0100

    TP GL

--- Genie log/Bibliotheque/Domain2Tests/EmpruntTests.cs
+++ Genie log/Bibliotheque/Domain2Tests/EmpruntTests.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Domain2Tests
+namespace Domain.Tests
 {
-    class EmpruntTests
+    [TestClass()]
+    public class EmpruntTests
     {
+        [TestMethod()]
+        public void EmpruntTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EmpruntTest1()
+        {
+            Assert.Fail();
+        }
     }
 }
